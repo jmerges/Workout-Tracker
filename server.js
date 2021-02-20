cc81096c7122b07f53e7066c133ad0090f650d21
jmerges bootcamp <jamesmerges1@gmail.com>
2021-02-20 01:04:00 -0800

    working put request

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const logger = require("morgan");
 const mongoose = require('mongoose');
+const mongojs = require('mongojs');
 
 const PORT = process.env.PORT || 3000;
 
@@ -15,10 +16,13 @@ app.use(express.json());
 
 app.use(express.static('public'));
 
+require("./routes/html-routes.js")(app);
+
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
 
 app.get("/api/workouts", (req, res) => {
     db.Workout.find({})
+    .populate("exercises")
     .then(dbWorkouts => {
         res.json(dbWorkouts);
     })
@@ -27,8 +31,20 @@ app.get("/api/workouts", (req, res) => {
     });
 });
 
-app.get("/api/workouts/range", (req, res) => {
+app.put("/api/workouts/:id", (req, res) => {
+    db.Exercise.create(req.body)
+        .then(exercise => db.Workout.findOneAndUpdate({_id: req.params.id}, { $push: { exercises: exercise._id } }, {new: true}))
+        .then(dbExercise => {
+            res.json(dbExercise);
+        });
+});
 
+app.post("/api/workouts", (req, res) => {
+    db.Workout.create(req.body)
+        .then(newWorkout => {
+            console.log(newWorkout);
+            res.json(newWorkout);
+        });
 });
 
 app.listen(PORT, () => {
